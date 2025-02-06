fe98b9880acc7dc12ef8a60773821eebbf4791d1
fatmaosama10 <152307482+fatmaosama10@users.noreply.github.com>
2025-02-06 08:48:12 +0200

    Update MealDetails.jsx

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./MealDetails.scss"; // قم بربط الملف SCSS
+import "./MealDetails.scss"; 
 
 const MealDetails = () => {
   const [meal, setMeal] = useState(null);
